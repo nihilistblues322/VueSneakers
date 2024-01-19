daaf06a8c5a37dd0175b6fd174de5337688eafab
msc <miserylovescompany228@gmail.com>
2024-01-19 13:34:42 +0600

    + drawer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
 import Header from './components/Header.vue'
+import CardList from './components/CardList.vue'
+import Drawer from './components/Drawer.vue'
 </script>
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+  <!-- <Drawer /> -->
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+      <CardList />
+    </div>
   </div>
 </template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('+')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :isFavorite="false"
+      :onClickAdd="onClickAdd"
+      :isAdded="true"
+    />
+ 
+  </div>
+</template>
