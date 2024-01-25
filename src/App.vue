9fdebf2bbb7b6b38f77dc992ca649328b4212bd9
msc <miserylovescompany228@gmail.com>
2024-01-25 14:57:11 +0600

    axios + mokky dev

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
 <script setup>
+import { onMounted, ref, watch, reactive } from 'vue'
+import axios from 'axios'
+
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
-import Drawer from './components/Drawer.vue'
+
+const items = ref([])
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy,
+      searchQuery: filters.searchQuery
+    }
+    const { data } = await axios.get(`https://604781a0efa572c1.mokky.dev/items`, {
+      params
+    })
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
+
 <template>
   <!-- <Drawer /> -->
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
@@ -12,10 +44,10 @@ import Drawer from './components/Drawer.vue'
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
 
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-mb outline-none">
-            <option value="">По названию</option>
-            <option value="">По цене (дешевые)</option>
-            <option value="">По цене (дорогие)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-mb outline-none">
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
 
           <div class="relative">
@@ -28,8 +60,9 @@ import Drawer from './components/Drawer.vue'
           </div>
         </div>
       </div>
-
-      <CardList />
+      <div mt-10>
+        <CardList :items="items" />
+      </div>
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,6 +1,10 @@
 <script setup>
 import Card from './Card.vue'
 
+defineProps({
+  items: Array
+})
+
 const onClickAdd = () => {
   alert('+')
 }
@@ -9,13 +13,12 @@ const onClickAdd = () => {
 <template>
   <div class="grid grid-cols-4 gap-5">
     <Card
-      title="Мужские кроссовки Nike Blazer Mid Suede"
-      imageUrl="/sneakers/sneakers-1.jpg"
-      :price="5000"
-      :isFavorite="false"
+      v-for="item in items"
+      :key="item.id"
+      :title="item.title"
+      :imageUrl="item.imageUrl"
+      :price="item.price"
       :onClickAdd="onClickAdd"
-      :isAdded="true"
     />
- 
   </div>
 </template>
